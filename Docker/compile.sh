f29ffae55a7523c8f6ad8b5b300c65870df73106
Martin Kennedy <hurricos@gmail.com>
2022-07-22 10:35:23 -0400

    fix: p1020: switch to the NAND defconfig
    This ensures we build U-Boot with a separate SPL / TPL.

--- Docker/compile.sh
+++ Docker/compile.sh
@@ -6,6 +6,6 @@ export SDK_DIR=/openwrt-sdk
 export STAGING_DIR=${SDK_DIR}/staging_dir
 export PATH=${STAGING_DIR}/toolchain-powerpc_8540_gcc-8.4.0_musl/bin:${PATH}
 
-make P1020RDB-PC_defconfig
+make P1020RDB-PC_NAND_defconfig
 
 make -j $(nproc) ARCH=powerpc CROSS_COMPILE=powerpc-openwrt-linux-musl- u-boot.bin
